a0cb8023e50043d2fa958db6a86276e3a2a391e1
tamam03 <tamam0308@gmail.com>
2022-05-11 07:57:02 +0000

    public/items

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,25 +1,57 @@
 class Public::ItemsController < ApplicationController
+
   def index
+    @items = Item.all
+   
+  end
+  
+  # 非公開投稿一覧
+  def privacy
+    @items = Item.all
+    @clse_items = @items.where(status:0)
+  end
+  
+  # 店舗スタッフのみ公開一覧
+  def clerk
   end
 
   def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
   end
 
   def create
-     binding.pry
+    # binding.pry
     @item = Item.new(item_params)
+    @item.user_id = current_user.id
     @item.save
     redirect_to public_items_path
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def destroy
+    @Item = Item.find(params[:id])
+    @Item.destroy
+    redirect_to  request.referer
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to public_item_path(item.id)
   end
 
 
   private
 
   def item_params
-    params.require(:item).permit(:text, :brand_id, :brand_name, :status, :user_id)
+    params.require(:item).permit(:text, :brand_id, :status)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
 
-  enum status: { close: 0, release: 1, only_clerk: 2 }
+  enum status: { 
+    close: 0, 
+    release: 1, 
+    only_clerk: 2 
+  }
 
   belongs_to :user
 
