8c598edcaa8f3c5b4b15e638eeee89d499edde3c
tamam03 <tamam0308@gmail.com>
2022-06-07 07:49:42 +0000

    admin/index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,21 +1,21 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
-    @items = Item.all
+    @items = Item.all.page(params[:page]).per(8)
   end
 
   def show
    @item = Item.find(params[:id])
   end
-  
+
   def user_index
     @user = User.find(params[:id])
     @items = @user.items
   end
-  
-  
+
+
   private
-  
+
   def item_params
     params.require(:item).permit(:text, :brand_id, :status, :item_image)
   end
