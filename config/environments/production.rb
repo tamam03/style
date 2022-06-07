7086d0f04c83253ff7f1c61d9664e4598b0403f9
tamam03 <tamam0308@gmail.com>
2022-06-07 07:24:31 +0000

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV["ACTION_MAILER_USER"],
+    password:             ENV["ACTION_MAILER_KEY"],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
 end
