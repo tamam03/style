84bf26e7fddb49ddef6f8467d29e2a1665bc6a4a
tamam03 <tamam0308@gmail.com>
2022-06-10 10:37:05 +0000

    message

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -1,17 +1,18 @@
 class Public::MessagesController < ApplicationController
-  
+
   def create
     message = Message.new(message_params)
     message.user_id = current_user.id
     if message.save
-      redirect_to room_path(message.room)
+      # binding.pry
+      redirect_to public_room_path(message.room)
     else
     end
   end
 
-  
+
   private
-  
+
   def message_params
     params.require(:message).permit(:sentence, :room_id)
   end
