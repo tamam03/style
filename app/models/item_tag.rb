f4f42cd3a22160194eecfbb873af0c25722ce094
kozyszoo <k.yamaoka0919@gmail.com>
2022-09-30 21:24:32 +0900

    中間テーブルを通したTagの保存

--- app/models/item_tag.rb
+++ app/models/item_tag.rb
@@ -1,21 +1,18 @@
 class ItemTag < ApplicationRecord
   has_many :tag_maps, dependent: :destroy, foreign_key: 'item_tag_id'
-  has_many :item_tags, through: :tag_maps
+  has_many :item, through: :tag_maps
 
   # タグ機能
   def save_tag(sent_tags)
-
-    current_tags = item_tags.pluck(:tag_name) unless item_tags.nil?
+    current_tags = item.pluck(:tag_name) unless item.nil?
     old_tags = current_tags - sent_tags
     new_tags = sent_tags - current_tags
-
-    old_tags.each do |old|
-      item_tags.delete ItemTag.find_by(tag_name: old)
-    end
+    tags = []
 
     new_tags.each do |new|
       new_item_tag = ItemTag.find_or_create_by(tag_name: new)
-      item_tags << new_item_tag
+      tags << new_item_tag
+      return tags
     end
   end
 end
