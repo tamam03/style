b3913b1fba9c00ccc55f2205a60b3c84be8bf4e6
tamam03 <tamam0308@gmail.com>
2022-10-03 06:42:17 +0000

    homes/top_new_item

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,5 +3,10 @@ class HomesController < ApplicationController
     if user_signed_in?
       redirect_to public_user_path(current_user)
     end
+
+    @new_items = Item.where(status: "release", user: User.where(is_user: false)).limit(6).order("created_at DESC")
+
   end
+
+
 end
