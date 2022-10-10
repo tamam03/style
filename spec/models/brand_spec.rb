41610cc0b72a5b376f58bd92152b0be9e2b1e8ee
tamam03 <tamam0308@gmail.com>
2022-10-10 10:48:45 +0000

    RSpec-brand

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,2 +1,10 @@
 require 'rails_helper'
-  
+
+RSpec.describe 'Adminモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    it '空欄でないこと' do
+      brand.brand_name = ''
+      is_expected.to eq false
+    end
+  end
+end
