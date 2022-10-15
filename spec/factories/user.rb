dc8a3f1b6c81583cd5a915649290c349af6af4b8
tamam03 <tamam0308@gmail.com>
2022-10-15 10:29:17 +0000

    spec/models/item_spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,24 +1,23 @@
 FactoryBot.define do
   factory :user do
-    japanese_name = Gimei.name
-    name { japanese_name.kanji }
-    name_kana { name.katakana }
-    brand
-    store
+    name { Faker::Lorem.characters(number: 5) }
+    name_kana { Faker:: Lorem.characters(number: 5) }
+    email { Faker::Internet.email }
+    password { 'password' }
+    password_confirmation { 'password' }
+    # japanese_name = Gimei.name
+    # name { japanese_name.kanji }
+    # name_kana { japanese_name.katakana }
+    
+    trait :true do
+      is_user { true }
+      nick_name { Faker::Lorem.characters(number: 5) }
+    end
+    
+    trait :false do
+      is_user { false }
+      brand
+      store
+    end
   end
 end
-
-# is_userがtrueの場合
-# name
-# name_kana
-# nick_name
-# mail
-# pass
-
-# false
-# name
-# name_kana
-# brand
-# store
-# mail
-# pass
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Itemモデルのテスト", type: :model do
   describe 'バリデーションのテスト' do
-    let(:user) { FactoryBot.create(:user) }
+    subject { item.valid? }
+
+    let(:user) { create(:user) }
     let!(:item) { build(:item, user_id: user.id) }
-    
-    subject { test_item.valid? }
-    let(:test_item) { item }
+
+    context 'textカラム' do
+      it '200字以下であること' do
+        item.text = Faker::Lorem.characters(number: 201)
+        is_expected.to eq false
+      end
+    end
   end
 end
-
-
-
-
-# text字数
