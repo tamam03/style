27db67fab6724c15e498c0885a5239daa569f382
tamam03 <tamam0308@gmail.com>
2022-10-22 08:31:23 +0000

    rspec/before_login

--- spec/system/_before_login_spec.rb
+++ spec/system/_before_login_spec.rb
@@ -5,34 +5,71 @@ describe "ログイン前テスト" do
     before do
       visit root_path
     end
-  
+
     context '表示内容の確認' do
       it 'root_pathが"/"であるか' do
         expect(current_path).to eq '/'
       end
-      
       it 'トップ画面にLog inボタンがあるか' do
         expect(page).to have_link "Log in", href: new_user_session_path
       end
-      
       it 'トップ画面にSign upボタンがあるか' do
         expect(page).to have_link "Sign up", href: new_user_registration_path
       end
-      
       it 'トップ画面にguestボタンがあるか' do
         expect(page).to have_link "Guest", href: public_guests_path
       end
     end
-  end
-  
-  describe 'about画面のテスト' do
-    before do
-      visit '/homes/about'
+
+    context 'ヘッダーのテスト:ログインをしていない場合' do
+      it 'styleリンクが表示される' do
+        expect(page).to have_link "style", href: root_path
+      end
+      it 'aboutリンクが表示される' do
+        expect(page).to have_link "About", href: homes_about_path
+      end
+      it 'guideリンクが表示される' do
+        expect(page).to have_link "Guide", href: homes_guide_path
+      end
     end
-    
-    it '表示内容の確認' do
-      expect(current_path).to eq '/homes/about'
+
+    context 'リンクの内容確認' do
+      subject { current_path }
+      
+      it 'styleをおすとルートページへ遷移' do
+        click_on 'style'
+        is_expected.to eq root_path
+      end
+      it 'aboutをおすとアバウトページへ遷移' do
+        click_on 'About'
+        is_expected.to eq '/homes/about'
+      end
+      it 'guideをおすとガイドページへ遷移' do
+        click_on 'Guide'
+        is_expected.to eq '/homes/guide'
+      end
     end
   end
 end
-  
+
+describe 'about画面のテスト' do
+  before do
+    visit '/homes/about'
+  end
+
+  it '表示内容の確認' do
+    expect(current_path).to eq '/homes/about'
+  end
+end
+
+describe 'guide画面のテスト' do
+  before do
+    visit '/homes/guide'
+  end
+
+  it '表示内容の確認' do
+    expect(current_path).to eq '/homes/guide'
+  end
+end
+
+
