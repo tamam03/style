1f8b37304d945658b6e8e4d1f6b281533320ac5c
tamam03 <tamam0308@gmail.com>
2022-10-25 13:09:58 +0000

    rspec/after_login

---
+++ spec/system/_after_login_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'ユーザーログイン後のテスト' do
+  before do
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'Log in'
+  end
+end
