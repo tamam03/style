513516b35ac88031596546fbf7f20203422a6b40
tamam03 <tamam0308@gmail.com>
2022-10-29 06:45:08 +0000

    after_login_spec.rb

--- spec/system/_after_login_spec.rb
+++ spec/system/_after_login_spec.rb
@@ -11,4 +11,25 @@ let(:user) { create(:user, nick_name: 'aaa', is_user: true) }
     click_button 'Log in'
   end
   
+  describe 'ログイン時ヘッダーのテスト' do
+    subject { current_path }
+    
+    context 'アイコン表示確認' do
+      it '検索ページアイコン：虫眼鏡' do
+        expect(page).to have_selector('.fas.fa-search')
+      end
+      it '新規投稿アイコン: +' do
+        expect(page).to have_selector('.far.fa-plus-square')
+      end
+      it '通知ページアイコン：ベル' do
+        expect(page).to have_selector('.fas.fa-bell')
+      end
+      it 'マイページアイコン：人' do
+        expect(page).to have_selector('.fas.fa-user')
+      end
+      it 'ログアウトアイコン：矢印' do
+        expect(page).to have_selector('.fas.fa-sign-out-alt')
+      end
+    end
+  end
 end
